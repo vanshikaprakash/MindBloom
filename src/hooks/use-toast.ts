f4ebcb5da87461497c8ee543bed1ad1e85ce88bc
Vanshika Prakash <vanshikaprakash05@gmail.com>
2025-08-15 23:33:19 +0530

    issues resolved

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,13 +1,12 @@
 
 "use client"
 
-// Inspired by react-hot-toast library
 import * as React from "react"
 
 import type {
   ToastActionElement,
   ToastProps,
-} from "../components/ui/toast"
+} from "@/components/ui/toast"
 
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
@@ -159,7 +158,7 @@ function toast({ ...props }: Toast) {
       ...props,
       id,
       open: true,
-      onOpenChange: (open) => {
+      onOpenChange: (open: boolean) => {
         if (!open) dismiss()
       },
     },
